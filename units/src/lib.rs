1be03faf1038e279256ee76d79f2dd85454aeb61
smsutherland <sagan@farthinghalearms.com>
2022-12-27 22:23:41 -0500

    add crate level lints

--- units/src/lib.rs
+++ units/src/lib.rs
@@ -1,3 +1,11 @@
+#![warn(
+    missing_docs,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc,
+    clippy::missing_safety_doc,
+    missing_debug_implementations
+)]
+
 mod quantity;
 mod unit;
 pub use unit::single::unit_defs::*;
