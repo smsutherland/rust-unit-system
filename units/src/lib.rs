9d525fe83dd6ec1ae2de7d479e00f86f8d60d268
smsutherland <sagan@farthinghalearms.com>
2022-12-28 00:00:31 -0500

    minimal crate-level documentation

--- units/src/lib.rs
+++ units/src/lib.rs
@@ -6,6 +6,10 @@
     missing_debug_implementations
 )]
 
+//! units (name pending) is a units library built around the idea that it should be
+//! simple to work with units in code. Working with units using this library should be similar to
+//! working with units in a normal mathematical context.
+
 mod quantity;
 mod unit;
 pub use unit::single::unit_defs::*;
