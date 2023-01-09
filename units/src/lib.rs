35b869a529eaee227376cd48f8e56b22af1617b3
smsutherland <sagan@farthinghalearms.com>
2023-01-09 17:30:06 -0500

    examples in crate documentation

--- units/src/lib.rs
+++ units/src/lib.rs
@@ -9,6 +9,32 @@
 //! units (name pending) is a units library built around the idea that it should be
 //! simple to work with units in code. Working with units using this library should be similar to
 //! working with units in a normal mathematical context.
+//!
+//! Create quantities by multiplying units with numbers.
+//! ```
+//! use units as u;
+//! let distance = 12. * u::cm;
+//! ```
+//!
+//! Arithmetic on quantities works as you would expect.
+//! ```
+//! use units as u;
+//! let distance = 12. * u::cm;
+//! let time = 2. * u::s;
+//! let velocity1 = distance / time;
+//! let velocity2 = 0.06 * u::m / u::s;
+//! assert_eq!(velocity1, velocity2);
+//! ```
+//!
+//! Converting between units can be done with the `to` method.
+//! ```
+//! use units as u;
+//! let distance = 12. * u::cm;
+//! let time = 2. * u::s;
+//! let velocity1 = (distance / time).to(u::m / u::s);
+//! let velocity2 = 0.06 * u::m / u::s;
+//! assert_eq!(velocity1, velocity2);
+//! ```
 
 mod quantity;
 mod unit;
